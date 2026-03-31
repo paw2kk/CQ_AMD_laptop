fb78c7f77bec2b95c64c157f2fb911ff6e6740f0
paw2k <wndxakk@gmail.com>
2026-03-31 20:38:52 +0700

    menambah fungsi update sks

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -150,6 +150,12 @@ namespace PraktikumADO
                 string query = "UPDATE MataKuliah SET SKS = 4 WHERE KodeMK = 'IF210101'";
 
                 cmd = new SqlCommand(query, conn);
+                
+                int hasil = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Jumlah baris terpengaruh : " + hasil);
+
+                conn.Close();
             }
         }
 }
